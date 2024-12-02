5400e26552de338449f704cb6823914ced8e995c
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-02 15:08:13 +0100

    Complete day 1 part 2

--- day1/main.cpp
+++ day1/main.cpp
@@ -36,8 +36,6 @@ int *splitstring(char line[]) {
     int num1 = std::atoi(num1_line);
     int num2 = std::atoi(num2_line);
 
-    std::cout << num1 << "\t" << num2 << std::endl;
-
     // Create integer list with resulst
     int* nums = new int[2];
     nums[0] = num1;
@@ -47,10 +45,9 @@ int *splitstring(char line[]) {
 }
 
 
-// Main function
-int main() {
+void part_one(std::string file_name) {
     // Read the file
-    std::vector<std::string> input_lines = read_file("input.txt");
+    std::vector<std::string> input_lines = read_file(file_name);
 
     // Variables
     int listLength = input_lines.size();
@@ -85,11 +82,64 @@ int main() {
         difference_sum += difference;
     }
 
-    std::cout << difference_sum << std::endl;
+    std::cout << "Answer 1: " << difference_sum << std::endl;
+}
+
+
+void part_two(std::string file_name) {
+    // Read the file
+    std::vector<std::string> input_lines = read_file(file_name);
+
+    // Variables
+    int listLength = input_lines.size();
+    std::vector<int> left_list;
+    std::vector<int> right_list;
+    int sim_score = 0;
+
+    // Get the numbers from the input lines
+    // Split on space
+    for (int i=0; i<listLength; i++) {
+        // Get line length and create appropriate char array size
+        int n = input_lines[i].length();
+        char arr[n+1];
+        
+        // Get the two nums out of the line
+        int *nums = splitstring(strcpy(arr, input_lines[i].c_str()));
+
+        // Add the numbers to the end of the vector
+        left_list.push_back(nums[0]);
+        right_list.push_back(nums[1]);
+
+        delete[] nums;
+    }
+
+    // Sort the list on numbers
+    sort(left_list.begin(), left_list.end());
+    sort(right_list.begin(), right_list.end());
+
+    // Loop through each element in the left list and compare to the right
+    for (int i=0; i<left_list.size(); i++) {
+        int instances = 0;
+        for (int j=0; j<right_list.size(); j++) {
+            if (left_list.at(i) < right_list.at(j)) break;
+            if (left_list.at(i) == right_list.at(j)) instances++;
+        }
+
+        sim_score += instances*left_list.at(i);
+    }
+
+    std::cout << "Answer 2: " << sim_score << std::endl;
+}
+
+
+// Main function
+int main() {
+    // Part 1
+    part_one("input.txt");
+
+    // Part 2
+    part_two("input.txt");
 
     // END
     return 0;
 }
-
-// Try 1: 944535002 is too high
-// Try 2: 1938424
