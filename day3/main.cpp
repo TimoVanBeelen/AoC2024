4bbe5adb580a7523346a988a7f64e1442c87df6d
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-03 10:19:09 +0100

    Start day 3

---
+++ day3/main.cpp
@@ -0,0 +1,19 @@
+// Day 3
+// Advent of Code 2024
+// 03-12-2024
+// Timo van Beelen
+
+
+// Packages needed in the program
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+
+// Main program
+int main(int argc, char *argv[]) {
+    // Put the main program structure here
+
+    // END
+    return 0;
+}
