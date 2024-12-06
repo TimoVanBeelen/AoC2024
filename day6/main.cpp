b3f67cc86eaaa6be3bf4ec9090978800822a9a39
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-06 13:09:50 +0100

    Find position of guard (day 6)

--- day6/main.cpp
+++ day6/main.cpp
@@ -41,6 +41,27 @@ char **strarr_to_chararr(std::vector<std::string> string_array) {
 }
 
 
+// Find the initial position of the guard
+int *find_pos(char **matrix, int height, int width) {
+    int *pos;                             // Create a variable of the returning position
+    for (int i=0; i<height; i++) {
+        for (int j=0; j<width; j++){        // Go through the whole matrix
+            if (matrix[i][j] == '^') {      // Guard is found
+                pos[0] = i;                 // Set its position
+                pos[1] = j;             
+            }
+        }
+    }
+    return pos;
+}
+
+
+// Move the guard
+char **update_matrix(char **matrix, int height, int width, int *pos) {
+    
+}
+
+
 // Run main code here
 int main(int argc, char *argv[]) {
     // Read file
@@ -51,6 +72,12 @@ int main(int argc, char *argv[]) {
     int width = input_lines[0].size();
     char **matrix = strarr_to_chararr(input_lines);
 
+    // Find the initial position of the guard
+    int *pos = find_pos(matrix, height, width);
+
     // Walk the guard where they need to go and replace each step with an X
+    int dir = 0;    // Set the walking direction, north = 0, west = 1, south = 2, east = 3
+    // Walk to next obstacle, check if border is reached, update the direction
+
     return 0;
 }
