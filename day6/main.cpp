9b564a0b7845c3cc3c81a5484aca37a8ad771717
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-06 12:58:14 +0100

    Day 6 create char matrix from input

--- day6/main.cpp
+++ day6/main.cpp
@@ -24,10 +24,33 @@ std::vector<std::string> read_file(std::string file_name) {
 }
 
 
-// Run main code here
-int main() {
-    // Read file
-    std::vector<std::string> input_lines =  read_file("input.txt");
+// Create a char matrix from a string array
+char **strarr_to_chararr(std::vector<std::string> string_array) {
+    int height = string_array.size();           // Height of the matrix
+    int width = string_array[0].size();         // Width of the matrix
 
+    char **char_array = new char*[height];                   // Create an output variable
+    for (int i = 0; i < height; i++) {
+        char_array[i] = new char[width];        // Create each row with the correct width
+
+        for (int j=0; j<width; j++)             // Set each character of the string to the correct char in the char array
+            char_array[i][j] = string_array[i].at(j);
+    }
+
+    return char_array;
+}
+
+
+// Run main code here
+int main(int argc, char *argv[]) {
+    // Read file
+    std::vector<std::string> input_lines =  read_file(argv[1]);
+
+    // Create a map: a matrix of characters, from the input lines
+    int height = input_lines.size();
+    int width = input_lines[0].size();
+    char **matrix = strarr_to_chararr(input_lines);
+
+    // Walk the guard where they need to go and replace each step with an X
     return 0;
 }
