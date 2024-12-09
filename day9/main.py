4ba521e03791b9be31737c33d11442d830040352
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-09 15:35:04 +0100

    Flinke beun, maar werkt -> blijheid

--- day9/main.py
+++ day9/main.py
@@ -13,6 +13,37 @@ def append_n_times(append_to: list, num: int, repeats: int):
         append_to.append(num)
     return append_to
 
+def part1(shuffled_sys, item, i):
+    # Insert from the back at free space
+    if item is None:
+        try:
+            del shuffled_sys[i]
+            shuffled_sys.insert(i, shuffled_sys[-1])
+            del shuffled_sys[-1]
+            return shuffled_sys
+        except IndexError: return shuffled_sys
+
+
+def get_block_size(lst: list, id: int):
+    size = 0
+    for item in lst:
+        if item == id: size +=1
+    return size
+
+
+def get_space_size(lst: list, index: int):
+    size = 0
+    for i in range(index, len(lst)):
+        if lst[i] == lst[index]: size +=1
+        else: break
+    return size
+
+
+def fill_space(lst: list, space_index: int, block_index: int, block_size: int):
+    lst[space_index:space_index+block_size] = lst[block_index:block_index+block_size]
+    for i in range(block_index, block_index+block_size): lst[i] = None 
+    return lst
+
 
 if __name__ == "__main__":
     # Read the input file
@@ -35,26 +66,44 @@ if __name__ == "__main__":
         else:
             shuffled_sys = append_n_times(shuffled_sys, None, int_arr[i])
 
-    i=0
-    for item in shuffled_sys:
-        # Remove trailing free space
-        for n in shuffled_sys[::-1]:
-            if n != None: break
-            del shuffled_sys[-1]
 
-        # Insert from the back at free space
-        if item is None:
-            try:
-                del shuffled_sys[i]
-                shuffled_sys.insert(i, shuffled_sys[-1])
-                del shuffled_sys[-1]
-            except IndexError: break
-        i += 1
+    not_changed = False
+    prev_item = -1
+    for item in shuffled_sys[::-1]:
+        if item is prev_item: continue
+        item_index = shuffled_sys.index(item)
+
+        # # if item is none
+        # if item is None:
+            
+        #     del shuffled_sys[-1]
+        #     continue
+        block_size = get_block_size(shuffled_sys, item)
+        
+        # Get information about space
+        space = 0
+        space_index = -1
+        for i in range(0, item_index):
+            if shuffled_sys[i] != None: continue
+            space = get_space_size(shuffled_sys, i)
+            if space >= block_size: 
+                space_index = i
+                break
+
+        # No large enough space is found
+        if space_index == -1: continue
+
+        shuffled_sys = fill_space(shuffled_sys, space_index, shuffled_sys.index(item), block_size)
+        prev_item = item
+
+        
+
+        
+
     
     runsum = 0
     for i in range(0, len(shuffled_sys)):
+        if shuffled_sys[i] is None: continue
         runsum += shuffled_sys[i]*i
 
     print(runsum)
-
-# Try 1: 6288599492129
