4fb403cf2d2cf4f7dc32de716b6c663deb8b77be
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-12 13:59:36 +0100

    Complete part2, apparantly memoization exists and I love it

--- day11/part2.cpp
+++ day11/part2.cpp
@@ -9,6 +9,9 @@
 #include <vector>
 #include <regex>
 #include <math.h>
+#include <chrono>
+
+using namespace std::chrono;
 
 
 // Read file and return string vector with all lines
@@ -60,55 +63,83 @@ ulong *split_num(ulong num, ulong nr_digs) {
 
 
 // Loop through new list and apply changes to a copy
-std::vector<ulong> change_stones(std::vector<ulong> input) {
+std::vector<ulong> change_stone(ulong item) {
     std::vector<ulong> new_iteration;     // The vector to be returned with all the new changes
-    for (ulong item: input) {
-        if (item == 0) {                  // Check if entry is 0 -> make 1
-            new_iteration.push_back(1);
-            continue;
-        }
-        ulong digs = num_digs(item);      // Get the number of digits 
-        if (digs%2==0) {            // Check if entry has even amount of digits -> split
-            ulong *arr = split_num(item, digs);
-            new_iteration.push_back(arr[0]);
-            new_iteration.push_back(arr[1]);
-        }
-        else {                            // Else multiply by 2024
-            ulong mult = 2024;
-            new_iteration.push_back(item*mult);
-        }
+
+    if (item == 0) {                  // Check if entry is 0 -> make 1
+        new_iteration.push_back(1);
+        return new_iteration;
+    }
+
+    ulong digs = num_digs(item);            // Get the number of digits 
+    if (digs%2==0) {                        // Check if entry has even amount of digits -> split
+        ulong *arr = split_num(item, digs);
+        new_iteration.push_back(arr[0]);
+        new_iteration.push_back(arr[1]);
+    } else {                                // Else multiply by 2024
+        ulong mult = 2024;
+        new_iteration.push_back(item*mult);
     }
     return new_iteration;
 }
 
 
+// Count a stone completely through iteratively, but with map
+ulong countStones(ulong stone, int remainingBlinks, std::map<std::pair<ulong, int>, ulong> *m) {
+    // If the amount of blinks is equal to 0, return 1 as count
+    if (remainingBlinks == 0) return 1;
+    if (remainingBlinks < 0) {
+        std::cout << "IMPOSSIBLE STATE" << '\n';
+        exit(1);
+    }
+
+    // Check if there is an entry in the map
+    std::pair<ulong, int> key{stone, remainingBlinks};
+    if (auto search = m->find(key); search !=m->end())
+        return search->second;                              // Key was found in map, return resulting value
+    
+    // Key is not in the map -> proceed to normal calculations
+    ulong count = 0;
+    std::vector<ulong> next_it = change_stone(stone);
+    for (ulong item: next_it) {
+        count += countStones(item, remainingBlinks-1, m);
+    }
+
+    // Add the result to the map
+    m->insert({key, count});
+
+    // return the result
+    return count;
+}
+
+
 int main(int argc, char *argv[]) {
-    // Check if there are enough arguments
+    // Check if there are enough arguments and start a timer
     if (argc < 3) return 0;
+    auto start = high_resolution_clock::now();  
 
     // Load the input data
     std::vector<std::string> input_lines =  read_file(argv[1]);
 
     // Convert string to array of digits
     std::vector<ulong> stones = stoia(input_lines[0]);
-    std::cout << "Starting: " << '\n';
-    for (ulong stone: stones)
-        std::cout << stone << " ";
-    std::cout << '\n';
 
     // Blink 25 times and update
     int blink_times = std::stoi(argv[2]);
-    time_t t;
-    time_t start = time(&t);
-    std::cout << "Starting time: " << start << '\n'; 
-    for (int i=0; i < blink_times; i++) {
-        std::cout << "Iteration/blink: " << i+1 << "/" << blink_times << '\n';
-        
-        // Change the stones according to the rules
-        stones = change_stones(stones); 
+    ulong stonesAmount = 0;
+    std::map<std::pair<ulong, int>, ulong> itermap;
+    for (ulong stone: stones) {
+        stonesAmount += countStones(stone, blink_times, &itermap);
     }
 
-    std::cout << "End time: " << time(&t)-start << "s" << '\n'; 
-    std::cout << "Answer: " << stones.size() << std::endl;
+    // Stop timer
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+
+    std::cout << "End time: " << duration.count() << '\n'; 
+    std::cout << "Answer: " << stonesAmount << std::endl;
     return 0;
 }
+
+// Try 1: 194295033 -> too low :(
+// Try 2: 229682160383225
