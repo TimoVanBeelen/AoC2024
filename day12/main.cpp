b3797b5675ed395e9ea089d495813d338da5b995
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-12 21:16:03 +0100

    Add switch statement, need to calculate the neighbours on the diagonals

--- day12/main.cpp
+++ day12/main.cpp
@@ -56,6 +56,42 @@ std::vector<std::pair<int, int>> pos_of_neighbours(map *m, int x, int y) {
 }
 
 
+// Get the position(s) of neighbouring field(s) with the same mark
+std::vector<std::pair<int, int>> pos_of_neighbours(map *m, int x, int y, char value) {
+    std::vector<std::pair<int, int>> result;    // Return a vector with the neighbours that need to be accounted for
+
+    // For all borderings of other fields, check mark and add to vector
+    if (x != 0 && m->get_val(x-1, y) == value)
+        result.push_back({x-1, y});
+    if (y != 0 && m->get_val(x, y-1) == value)
+        result.push_back({x, y-1});
+    if (x != m->get_height()-1 && m->get_val(x+1, y) == value)
+        result.push_back({x+1, y});
+    if (y != m->get_width()-1 && m->get_val(x, y+1) == value)
+        result.push_back({x, y+1});
+
+    return result;
+}
+
+
+// Get the position(s) of neighbouring field(s) with another mark
+std::vector<std::pair<int, int>> next_with_other_mark(map *m, int x, int y, char value) {
+    std::vector<std::pair<int, int>> result;    // Return a vector with the neighbours that need to be accounted for
+
+    // For all borderings of other fields, check mark and add to vector
+    if (x != 0 && m->get_val(x-1, y) != value)
+        result.push_back({x-1, y});
+    if (y != 0 && m->get_val(x, y-1) != value)
+        result.push_back({x, y-1});
+    if (x != m->get_height()-1 && m->get_val(x+1, y) != value)
+        result.push_back({x+1, y});
+    if (y != m->get_width()-1 && m->get_val(x, y+1) != value)
+        result.push_back({x, y+1});
+
+    return result;
+}
+
+
 // Itteratively check each field and return the (area, perim) pair
 std::pair<int, int> calc_plot_cost(map *m, int x_start, int y_start) {
     // Set up variables
@@ -77,6 +113,54 @@ std::pair<int, int> calc_plot_cost(map *m, int x_start, int y_start) {
 
     return {area, perim};
 }
+
+
+// Calculate how much extra perimiter is needed
+int extra_perimiter(map *m, int x, int y, std::vector<std::pair<int, int>> neighbours) {
+    // Get the 'anti' location
+    std::vector<std::pair<int, int>> diff_neighbours = next_with_other_mark(m, x, y, m->get_val(x, y));
+    for (std::pair<int, int> )
+
+    // Switch dependent on diagonals
+    switch (neighbours.size())
+    {
+    case 1:
+        // Either 0, 2 or 3
+        break;
+    case 3:
+        // Either -4, -2 or 0
+        break;
+    case 4:
+        return -4;
+    default:
+        break;
+    }
+    return 0;
+}
+
+
+// Itteratively check each field and return the (area, perim) pair
+std::pair<int, int> buy_in_bulk(map *m, int x_start, int y_start) {
+    // Set up variables
+    int area = 1;           // Area of the current position
+    int perim = 0;          // Each field has a perimiter of 4, start with that
+    m->is_visited[x_start][y_start] = true;     // We have visited, so true  
+
+    // Check where the neighbours of the same mark are and use that to get the addition of perimiter
+    std::vector<std::pair<int, int>> neighbours = pos_of_neighbours(m, x_start, y_start);
+    perim += extra_perimiter(m, x_start, y_start, neighbours);
+
+    // While there is a neighbour with same mark, add to area and perimiter
+    for (std::pair<int, int> neighbour: neighbours){
+        if (m->is_visited[neighbour.first][neighbour.second]) continue; // don't count double
+        std::pair<int, int> itres;
+        itres = calc_plot_cost(m, neighbour.first, neighbour.second);   // Get the result of the iteration
+        area += itres.first;                                        
+    }      
+
+    return {area, perim};
+}
+
 
 
 
