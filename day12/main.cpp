bce8a076b4f247827a23dd8310e948c234382ce5
TimoVanBeelen <timovbeelen@hotmail.com>
2024-12-12 20:44:26 +0100

    Part 1 refactoring

--- day12/main.cpp
+++ day12/main.cpp
@@ -11,20 +11,6 @@
 #include "map.h"
 
 
-// struct to keep track of data for each different plot
-struct field {
-    int area = 0;
-    int perim = 0;
-    char c = '.';
-
-    field(char val) {
-        this->c = val;
-    }
-    field() {}
-};
-
-
-
 // Read file and return string vector with all lines
 std::vector<std::string> read_file(std::string file_name) {
     std::vector<std::string> input_data;
@@ -48,67 +34,6 @@ void fill_fields(map *m, std::vector<std::string> input) {
             m->is_visited[i][j] = false;
         }
     }
-}
-
-
-// Check if the plot with a certain letter already exists
-bool field_exists(std::vector<field> *flds, char val) {
-    for (size_t i=0; i<flds->size(); i++) {
-        if (flds->at(i).c == val) return true;
-    }
-    return false;
-}
-
-
-// Find the field data and also that of its neighbours
-void set_field_data(std::vector<field> *flds, map m, int x, int y) {
-    // Check if the field exists, otherwise append it to the vector
-    char value = m.get_val(x, y);
-    if (!field_exists(flds, value))
-        flds->push_back(field(value));
-
-    int index = -1;          // Now we can call the field by its index
-    for (size_t i=0; i<flds->size(); i++) {
-        if (flds->at(i).c == value) {
-            index = i;
-            break;
-        }
-    }
-    
-    // Increase the area by 1
-    flds->at(index).area++;
-
-    // Check if we are at a border, then add an extra fence
-    if (x == 0 || y == 0 || x == m.get_height()-1 || y == m.get_width()-1) {
-        if (x == y || (x == m.get_height()-1 && y == 0) || (x == 0 && y == m.get_width()-1)) {
-            flds->at(index).perim++;    // Corners have two border fences
-        }
-        flds->at(index).perim += 1;
-    }
-
-    // For all borderings of other fields, add a fence
-    if (x != 0 && m.get_val(x-1, y) != value)
-        flds->at(index).perim++;
-    if (y != 0 && m.get_val(x, y-1) != value)
-        flds->at(index).perim++;
-    if (x != m.get_height()-1 && m.get_val(x+1, y) != value)
-        flds->at(index).perim++;
-    if (y != m.get_width()-1 && m.get_val(x, y+1) != value)
-        flds->at(index).perim++;
-
-}
-
-
-// Fill the field data with area and perimiter
-std::vector<field> get_field_data(map m) {
-    std::vector<field> result;
-    for (int i=0; i<m.get_height(); i++) {
-        for (int j=0; j<m.get_width(); j++){
-            // For each place on the map add to a field
-            set_field_data(&result, m, i, j);
-        }
-    }
-    return result;
 }
 
 
@@ -167,9 +92,6 @@ int main(int argc, char *argv[]) {
     map plots(input_lines.size(), input_lines[0].size());
     fill_fields(&plots, input_lines);
 
-    // Create a vector of fields and add to those the size and perimiter
-    std::vector<field> fields = get_field_data(plots);
-
     // Print some information on each field
     int fence_cost = 0;
     for (int i=0; i<plots.get_height()*plots.get_width(); i++) {
@@ -188,11 +110,6 @@ int main(int argc, char *argv[]) {
         std::cout << "Resulting in costs of: " << APpair.first*APpair.second << '\n' << '\n';
         fence_cost += APpair.first * APpair.second;
     }
-
-    // for (field f: fields){
-    //     std::cout << f.c << " " << f.area << " " << f.perim << '\n';
-    //     fence_cost += f.area*f.perim;
-    // }
 
     std::cout << "Answer: " << fence_cost << std::endl;
     return 0;
